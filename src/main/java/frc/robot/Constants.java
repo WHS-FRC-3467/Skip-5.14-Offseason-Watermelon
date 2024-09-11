0c7bcaa6928b7a0f71ecd522647434c38c535838
Noah-H3467 <nhaskell26@team3467.org>
2024-09-10 20:57:26 -0400

    intake roller CAN id is 15 for now

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+/** Add your docs here. */
+public class Constants {
+
+
+    public static final class ExampleCTREMotorConfig { 
+        public static final int ID_Motor = 0;
+
+        public static TalonFXConfiguration motorConfig() {
+            TalonFXConfiguration m_configuration = new TalonFXConfiguration();
+
+            m_configuration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            m_configuration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+            m_configuration.Voltage.PeakForwardVoltage = 12.0;
+            m_configuration.Voltage.PeakReverseVoltage = -12.0; 
+
+            m_configuration.CurrentLimits.SupplyCurrentLimit = 20; 
+            m_configuration.CurrentLimits.SupplyCurrentThreshold = 40;
+            m_configuration.CurrentLimits.SupplyTimeThreshold = 0.1; 
+            m_configuration.CurrentLimits.SupplyCurrentLimitEnable = true; 
+            m_configuration.CurrentLimits.StatorCurrentLimit = 70;
+            m_configuration.CurrentLimits.StatorCurrentLimitEnable = true; 
+
+            return m_configuration;
+        }
+    }
+    
+    public static class IntakeRollerConstants {
+        
+        public static final int ID_IntakeRoller = 15;
+
+        public static TalonFXConfiguration motorConfig() {
+            TalonFXConfiguration m_configuration = new TalonFXConfiguration();
+
+            m_configuration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            m_configuration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            m_configuration.Voltage.PeakForwardVoltage = 12.0;
+            m_configuration.Voltage.PeakReverseVoltage = -12.0; 
+
+            m_configuration.CurrentLimits.SupplyCurrentLimit = 20; 
+            m_configuration.CurrentLimits.SupplyCurrentThreshold = 40;
+            m_configuration.CurrentLimits.SupplyTimeThreshold = 0.1; 
+            m_configuration.CurrentLimits.SupplyCurrentLimitEnable = true; 
+            m_configuration.CurrentLimits.StatorCurrentLimit = 120;
+            m_configuration.CurrentLimits.StatorCurrentLimitEnable = true; 
+
+            return m_configuration;
+        }
+
+        public static final double k_INTAKE_FWD_SPEED = -0.6;
+        public static final double k_INTAKE_REV_SPEED = 0.3;
+    }
+}
--- src/main/java/frc/robot/subsystems/IntakeRollers.java
+++ src/main/java/frc/robot/subsystems/IntakeRollers.java
@@ -4,50 +4,94 @@
 
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeRollerConstants;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import java.util.function.DoubleSupplier;
 
 public class IntakeRollers extends SubsystemBase {
 
-  @RequiredArgsConstructor
-  @Getter
-  public enum State {
-    ON(() -> 1.0),
-    OFF(() -> 0.0);
+    /** Intake subsystem singleton. For superstructure. */
+    private static IntakeRollers instance = null;
 
-    private final DoubleSupplier outputSupplier;
+    @RequiredArgsConstructor
+    @Getter
+    public enum State{
 
-    private double getStateOutput() {
-      return outputSupplier.getAsDouble();
+        FWD (() -> IntakeRollerConstants.k_INTAKE_FWD_SPEED),
+        REV (() -> IntakeRollerConstants.k_INTAKE_REV_SPEED),
+        OFF(() -> 0.0);
+
+        private final DoubleSupplier outputSupplier;
+
+        private double getStateOutput() {
+            return outputSupplier.getAsDouble();
+        }
     }
-  }
 
-  @Getter
-  @Setter
-  private State state = State.OFF;
+    @Getter
+    @Setter
+    private State state = State.OFF;
 
-  /** Creates a new SimpleSubsystem. */
-  public IntakeRollers() {
+    // Initalize Motors and Beam Break
+    TalonFX m_roller = new TalonFX(IntakeRollerConstants.ID_IntakeRoller);
 
-  }
+    // For superstructure
+    /**
+    * Returns the intake subsystem instance.
+    *
+    * @return the intake subsystem instance.
+    */
+    public static IntakeRollers getInstance() {
+        if (instance == null) {
+            instance = new IntakeRollers();
+        }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-
-    if (state == State.OFF) {
-      // m_intakeMotor.setControl(m_brake);
-    } else {
-      // m_intakeMotor.setControl(m_percent.withOutput(state.getStateOutput()));
+        return instance;
     }
-  }
 
-  public Command setStateCommand(State state) {
-    return runOnce(() -> this.state = state);
-  }
+    /** Creates a new IntakeSubsystem. */
+    public IntakeRollers() {
+        
+        m_roller.getConfigurator().apply(IntakeRollerConstants.motorConfig());
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        displayInfo(true);
+
+        m_roller.set(state.getStateOutput());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+
+    /**
+     * Example command factory method. Periodic tells the intake to run according to the state
+     *
+     * @return a command setting the intake state to the argument
+     */
+    public Command setStateCommand(State state) {
+        return startEnd(() -> this.state = state, () -> this.state = State.OFF);
+    }
+
+    public void displayInfo(boolean debug) {
+        if (debug) {
+            SmartDashboard.putString("Intake Roller State", getState().toString());
+            SmartDashboard.putNumber("Intake Roller Setpoint", state.getStateOutput());
+            SmartDashboard.putNumber("Intake Roller speed", m_roller.getVelocity().getValueAsDouble());
+            SmartDashboard.putNumber("Intake Roller Current Draw", m_roller.getSupplyCurrent().getValueAsDouble());
+        }
+    }
 }
