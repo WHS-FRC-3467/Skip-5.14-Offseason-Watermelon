f4bf55335c96272bf4d1836ede0f7f6183752edf
SH-3467 <shaskell28@team3467.org>
2024-09-12 21:12:12 -0400

    Worked on Motion Magic

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,66 +6,134 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.RobotState;
+import frc.robot.Constants.ElevatorConstants;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+
 
 public class Elevator extends SubsystemBase {
 
+  //This is code from the W8 Library's Elevator subsystem
+
   @RequiredArgsConstructor
   @Getter
-  public enum State {
-    HOME(() -> 0.0),
-    OUT(() -> 90.0);
+  public enum State { //0 angle is straight horizontal, -20 is on the hardstops
+    COLLECT(() -> 0.0, () -> 0.0),
+    AMP(() -> 90.0, () -> 90.0),
+    TRAP(() -> 50.0, () -> 50.0),
+    // CUSTOM(() -> 5); //I'm not sure if this is needed
 
-    private final DoubleSupplier outputSupplier;
+    private final DoubleSupplier outputSupplierUno;
+    private final DoubleSupplier outputSupplierDos; // I believe there are two motors that move the elevator
 
     private double getStateOutput() {
-      return outputSupplier.getAsDouble();
+      return Units.degreesToRadians(outputSupplierUno.getAsDouble());
+    }
+    private double getStateOutput() {
+      return Units.degreesToRotations(outputSupplierDos.getAsDouble());
     }
   }
 
   @Getter
   @Setter
-  private State state = State.HOME;
+  private State state = State.COLLECT;
 
-  private final double upperLimitDegrees = 180;
-  private final double lowerLimitDegrees = 0;
-  private final double maxVelocity = 1;
-  private final double maxAcceleration = 1;
-  private ProfiledPIDController pidController = new ProfiledPIDController(0, 0, 0,
-      new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration));
-  private double goalAngle;
-  private double currentAngle;
-  private SimpleMotorFeedforward ff = new SimpleMotorFeedforward(0, 0);
+
+  //Create a on RIO Profile PID Controller, adding constraints to limit max vel and accel 
+  private ProfiledPIDController pidController = new ProfiledPIDController(18, 0, 0.2,);
+      //new TrapezoidProfile.Constraints(ElevatorConstants.maxVelocity, ElevatorConstants.maxAcceleration));
+
+  private ElevatorFeedforward ff = new ElevatorFeedforward(0.5, 0.4, 2.5, 0.01);
+
+ 
   private double output = 0;
 
-  /** Creates a new ComplexSubsystem. */
+  TalonFX m_ElevatorMotor = new TalonFX(ElevatorConstants.ID_ElevatorLeader);
+  TalonFX m_ElevatorFollowerMotor = new TalonFX(ElevatorConstants.ID_ElevatorFollower);
+
+  private VoltageOut m_VoltageOutput = new VoltageOut(0.0);
+  private NeutralOut m_neutralOut = new NeutralOut();
+
+  private DutyCycleEncoder m_encoder = new DutyCycleEncoder(ElevatorConstants.ID_ElevatorAbsEncoder);
+
+  private Debouncer m_debounce = new Debouncer(.1);
+  
+
+
+  /** Creates a new ElevatorSubsystem. */
   public Elevator() {
+
+    m_ElevatorMotor.getConfigurator().apply(ElevatorConstants.motorConfig());
+    m_ElevatorFollowerMotor.getConfigurator().apply(ElevatorConstants.motorConfig());
+    m_ElevatorFollowerMotor.setControl(new Follower(ElevatorConstants.ID_ElevatorLeader, true));
+
+    m_encoder.setDutyCycleRange(1.0/1025.0, 1024.0/1025.0); 
+    m_encoder.setDistancePerRotation(2*Math.PI); //Define 1 full rotation to be 2 Pi
+    m_encoder.setPositionOffset(0.50666666); //Encoder offset to make horizontal report 0 deg
+    
+
+    pidController.setTolerance(ElevatorConstants.tolerance);
+    pidController.setIZone(100); // TODO: Figure out actual value
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    goalAngle = MathUtil.clamp(state.getStateOutput(), lowerLimitDegrees, upperLimitDegrees);
-
-    if (state == State.HOME && pidController.atGoal()) {
-      // motor.setControl(Neutral)
+    goalAngle = MathUtil.clamp(state.getStateOutput(), ElevatorConstants.lowerLimit, ElevatorConstants.upperLimit);
+    pidController.setGoal(goalAngle);
+    
+    if (state == State.COLLECT && atGoal()) {
+      m_ElevatorMotor.setControl(m_neutralOut);
     } else {
-      output = pidController.calculate(currentAngle, goalAngle) + ff.calculate(0, 0);
-      // motor.setControl(output);
+      output = pidController.calculate(m_encoder.getDistance()) + ff.calculate(goalAngle, 0);
+      m_ElevatorMotor.setControl(m_VoltageOutput.withOutput(output));
     }
+
+    displayInfo(true);
 
   }
 
+  
+  public boolean atGoal() {
+    //return m_debounce.calculate(MathUtil.isNear(goalAngle, m_encoder.getDistance(), ElevatorConstants.tolerance));
+    return m_debounce.calculate(pidController.atGoal());
+    //return pidController.atGoal();
+  }
+
   public Command setStateCommand(State state) {
-    return runOnce(() -> this.state = state);
+    return startEnd(() -> setState(state), () -> setState(State.COLLECT))
+        .withName("Set Elevator State: " + state.toString());
+  }
+
+  private void displayInfo(boolean debug) {
+    if (debug) {
+      SmartDashboard.putString("Elevator State ", state.toString());
+      SmartDashboard.putNumber("Elevator Setpoint ", Units.radiansToDegrees(goalAngle));
+      SmartDashboard.putNumber("Elevator Angle ", Units.radiansToDegrees(m_encoder.getDistance()));
+      SmartDashboard.putBoolean("Elevator at Goal?", atGoal());
+      SmartDashboard.putNumber("Elevator Current Draw", m_ElevatorMotor.getSupplyCurrent().getValueAsDouble());
+      SmartDashboard.putData("Elevator PID",pidController);
+    }
+
   }
 }
